6b51c5031b5fecbd846af87b9dd9896accdd76e6
Bulat <imaevbulat16@gmail.com>
2023-11-08 14:23:28 +0500

    add jest, babel preset, add first test

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -45,5 +45,25 @@ export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
         ],
     };
 
-    return [typescriptLoader, cssLoader, svgLoader, fileLoader];
+    const babelLoader = {
+        test: /\.(js|jsx|tsx)$/,
+        exclude: /node_modules/,
+        use: {
+            loader: 'babel-loader',
+            options: {
+                presets: ['@babel/preset-env'],
+                plugins: [
+                    [
+                        'i18next-extract',
+                        {
+                            locales: ['ru', 'en'],
+                            keyAsDefaultValue: true,
+                        },
+                    ],
+                ],
+            },
+        },
+    };
+
+    return [babelLoader, typescriptLoader, cssLoader, svgLoader, fileLoader];
 }
--- src/test.ts
+++
@@ -1,4 +0,0 @@
-export function someFunc(arg: number) {
-    console.log('test func text 3453534534asdsdfds');
-    return '';
-}
